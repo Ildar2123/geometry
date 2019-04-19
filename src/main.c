e909f5357a1400da9f92bbd6e37e8b38f3258300
Ildar2123 <ildar@DESKTOP-2BPBJTP.localdomain>
2019-04-19 14:58:05 +0700

    main

--- src/main.c
+++ src/main.c
@@ -5,42 +5,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+int main()
+{
   FILE *myfile;
-    myfile = fopen("coordinates.txt", "r");
-    if (myfile == NULL) {
+  myfile = fopen("coordinates.txt", "r");
+  if (myfile == NULL)
+  {
     printf("No file\n");
     return 1;
-    }
-    printf("You input:\n");
-    int capacity = 100;
-    int size = 0;
-    char arr[256];
-    int i = 0;
-    Figure* new = (Figure*)malloc(sizeof(Figure) * capacity);
-    if (new == NULL) {
+  }
+  printf("You input:\n");
+  int capacity = 100;
+  int size = 0;
+  char arr[256];
+  int i = 0;
+  Figure *new = (Figure *)malloc(sizeof(Figure) * capacity);
+  if (new == NULL)
+  {
     printf("Alloceted error\n");
     return 1;
-    }
-    while ((arr[i++] = fgetc(myfile)) != EOF) {
-    if (size < capacity) {
-      if (arr[i - 1] == '\n') {
+  }
+  while ((arr[i++] = fgetc(myfile)) != EOF)
+  {
+    if (size < capacity)
+    {
+      if (arr[i - 1] == '\n')
+      {
         arr[i - 1] = '\0';
-        printf("-----str %d-----\n", size + 1);
-        if (!(First_Character(arr)) && !(Parser(&new[size], arr))) {
+
+        if (!(First_Character(arr)) && !(Parser(&new[size], arr)))
+        {
           Work(&new[size]);
         }
         size++;
         i = 0;
       }
-    } else {
+    }
+    else
+    {
       capacity *= 2;
       new = (Figure *)realloc(new, capacity * sizeof(double));
-      if (new == NULL) {
+      if (new == NULL)
+      {
         printf("Alloceted error\n");
         return 1;
       }
     }
-    }
-    return 0;
+  }
+  return 0;
 }
